72dba0fae971259830fe2dc689e3ec83acaaa5d8
Runar Fredagsvik <runarsf@protonmail.com>
2020-10-19 00:05:23 +0200

    delete readme

--- preonic/keymap.c
+++ preonic/keymap.c
@@ -28,10 +28,10 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_preonic_grid( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,  \
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL,   \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_BSLS, KC_DEL,   \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENTER, \
-    KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT   \
+    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT   \
   ),
 
   /*
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_LOWER] = LAYOUT_preonic_grid( \
     S(KC_EQL), KC_EXLM, ALGR(KC_2), KC_HASH, ALGR(KC_4), KC_PERC, S(KC_6), S(KC_7), ALGR(KC_RBRC), S(KC_0),    KC_MINS, _______, \
-    KC_TILD,   KC_EXLM, KC_AT,      KC_HASH, KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,       KC_LPRN,    KC_RPRN, KC_DEL, \
+    KC_TILD,   KC_EXLM, KC_AT,      KC_HASH, KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,       KC_LPRN,    KC_RPRN, KC_DEL,  \
     _______,   _______, KC_NUHS,    _______, _______,    _______, _______, _______, ALGR(KC_8),    ALGR(KC_9), _______, _______, \
     _______,   _______, _______,    _______, _______,    _______, _______, _______, _______,       _______,    _______, _______, \
     _______,   _______, _______,    _______, _______,    KC_SPC,  KC_SPC,  _______, KC_HOME,       KC_PGDN,    KC_PGUP, KC_END   \
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_preonic_grid( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,          KC_8,       KC_9,       KC_0,       KC_LBRC , \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,          KC_8,       KC_9,       KC_0,       KC_LBRC , \
-    _______, _______, KC_NUHS, _______, _______, _______, _______, S(ALGR(KC_7)), ALGR(KC_7), ALGR(KC_0), KC_QUOT,    KC_SCLN,  \
+    _______, _______, KC_NUHS, _______, _______, _______, _______, S(ALGR(KC_7)), ALGR(KC_7), ALGR(KC_0), KC_SCLN,    KC_QUOT,  \
     _______, _______, _______, _______, _______, _______, _______, _______,       KC_NUBS,    S(KC_NUBS), S(KC_RBRC), _______,  \
     _______, _______, _______, _______, ADJUST,  KC_SPC,  KC_SPC,  ADJUST,        KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END    \
   ),
